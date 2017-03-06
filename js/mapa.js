70a0090b9000e270dcc1beb850f2010835b00c8d
SandraSILK <mrs0sandwich@gmail.com>
2017-03-06 21:28:14 +0100

    first commit

---
+++ js/mapa.js
@@ -0,0 +1,18 @@
+﻿	
+	function map() {
+
+		var loc = {lat:54.409771, lng:18.571130};
+		var map = new google.maps.Map (
+			document.getElementById('map'), {
+				zoom: 16,
+				center: loc
+			});
+
+		//var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+		var image = 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTLyozHyKdACnR5oKsseuKcIJZDptuPJskmho1mg3RytNjKexJfAg';
+		var marker = new google.maps.Marker ({
+			position: loc,
+			map: map,
+			icon: image,
+		});
+	}
---
+++ js/slider.js
@@ -0,0 +1,28 @@
+﻿
+var numer = Math.floor(Math.random()*3)+1;  //zmienna globalna, floor zaokrągla w dół, random - losuje od 0 do 3, ale na koncu dodajemy 1 wiec od 1 do 3
+
+function zmienSlajd()  {
+
+	numer++; if (numer>3)numer=1; //number++ zmieniaj slajdy na kolejne, if do 3, a później zacznij od 1 
+	var textList = ["laser", "maska", "masaż"] 
+
+	var plik = "<img src=\"js/slajdy/slajd" + numer + ".jpg \" />" ;
+	//for  (var i = text.length[i]; i>0; i++);
+		text = textList[numer-1]; 
+
+/*	if(numer == 1)
+	{
+		var text = "laser"
+	} 
+	else if(numer == 2)
+	{
+		 var text = "maska"
+	} else { 
+		var text = "masaż"}; */
+
+	document.getElementById("slajder").innerHTML = plik;
+	document.getElementById("napis").innerHTML = text;
+
+	setTimeout("zmienSlajd()",5000);
+}
+
